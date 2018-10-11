c29cb6bb8c8fee42d7afa6e947304c8725c1d8e7
Krasimira Mashova <krasimira.mashova@sap.com>
2018-10-11 16:08:19 +0300

    created a service that returns list of users and users-id

--- server/app.js
+++ server/app.js
@@ -1,47 +1,53 @@
 const express = require('express');
 const app = express();
 
-var people = [
+var peopleData = [
     {
-        id: 1,
+        id: 0,
         name: 'Alden',
         age: 32
     },
     {
-        id: 2,
+        id: 1,
         name: 'Billy',
         age: 27
     },
     {
-        id: 3,
+        id: 2,
         name: 'Clark',
         age: 38
     },
     {
-        id: 4,
+        id: 3,
         name: 'Kayla',
         age: 19
     },
     {
-        id: 5,
+        id: 4,
         name: 'Vanessa',
         age: 30
     },
     {
-        id: 6,
+        id: 5,
         name: 'Mariah',
         age: 22
     }
 ];
 
-console.log(people);
+console.log(peopleData);
 
 app.get('/test', function (req, res) {
     res.send('Hello World!')
 });
 
 app.get('/api/users', function (req, res) {
-    res.json(people);
+    res.json(peopleData);
+});
+
+app.get('/api/users/:id', function (req, res) {
+    var peopleId = req.params.id;
+
+    res.json(peopleData[peopleId]);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
