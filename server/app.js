38229b07c9c3f35f32807d244e2d7b2134170c7d
Krasimira Mashova <krasimira.mashova@sap.com>
2018-10-12 19:53:26 +0300

    improved code - returning a user by given name as a parameter

--- server/app.js
+++ server/app.js
@@ -1,53 +1,64 @@
 const express = require('express');
 const app = express();
 
-var peopleData = [
+const users = [
     {
-        id: 0,
+        id: 123,
         name: 'Alden',
         age: 32
     },
     {
-        id: 1,
+        id: 456,
         name: 'Billy',
         age: 27
     },
     {
-        id: 2,
+        id: 789,
         name: 'Clark',
         age: 38
     },
     {
-        id: 3,
+        id: 987,
         name: 'Kayla',
         age: 19
     },
     {
-        id: 4,
+        id: 654,
         name: 'Vanessa',
         age: 30
     },
     {
-        id: 5,
+        id: 321,
         name: 'Mariah',
         age: 22
     }
 ];
 
-console.log(peopleData);
+console.log(users);
 
 app.get('/test', function (req, res) {
     res.send('Hello World!')
 });
 
 app.get('/api/users', function (req, res) {
-    res.json(peopleData);
+    res.json(users);
 });
 
 app.get('/api/users/:id', function (req, res) {
-    var peopleId = req.params.id;
+    const id = parseInt(req.params.id);
+    const user = users.find(function (element) {
+        return element.id === id;
+    });
 
-    res.json(peopleData[peopleId]);
+    if (user) {
+        res.json(user);
+    } else {
+        res.json({
+            success: false,
+            message: 'No such user with id: ' + id
+        });
+    }
+
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(4000, () => console.log('Example app listening on port 4000!'));
